ed94bef66d755dd44e1eb7008aaa872e1e1d6b43
AlexandreDresch <connectWithAlexandre@proton.me>
2024-05-29 20:59:28 -0300

    feat: add Header & TotalBalance to home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,26 @@
+import Header from "@/components/header";
+import TotalBalance from "@/components/total-balance";
+
 export default function Home() {
+  const loggedIn = { firstName: "John" };
   return (
-    <div>page</div>
-  )
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <Header
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalance
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1000.5}
+          />
+        </header>
+      </div>
+    </section>
+  );
 }
