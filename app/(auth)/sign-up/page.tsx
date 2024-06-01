d8374f0fb12e0918ee263a4399407188a30ee53e
AlexandreDresch <connectWithAlexandre@proton.me>
2024-06-01 00:04:22 -0300

    feat: add AuthForm to SignUp page

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,9 @@
+import AuthForm from "@/components/auth-form";
+
 export default function SignUp() {
   return (
-    <div>page</div>
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up"/>
+    </section>
   )
 }
