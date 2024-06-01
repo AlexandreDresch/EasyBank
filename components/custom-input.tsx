8ebed04a93dba45068bd15495905c4d1548b9d46
AlexandreDresch <connectWithAlexandre@proton.me>
2024-05-31 23:46:06 -0300

    feat: update CustomInput types

--- components/custom-input.tsx
+++ components/custom-input.tsx
@@ -7,9 +7,11 @@ import { z } from "zod";
 import { HTMLInputTypeAttribute } from "react";
 import { authFormSchema } from "@/lib/utils";
 
+const formSchema = authFormSchema("sign-up")
+
 interface CustomFormProps {
-  control: Control<z.infer<typeof authFormSchema>>;
-  name: FieldPath<z.infer<typeof authFormSchema>>;
+  control: Control<z.infer<typeof formSchema>>;
+  name: FieldPath<z.infer<typeof formSchema>>;
   label: string;
   placeholder: string;
   type?: HTMLInputTypeAttribute;
