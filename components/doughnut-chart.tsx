af998551fbd7fe0e095208dc14b46e2d71d1d394
AlexandreDresch <connectWithAlexandre@proton.me>
2024-06-12 23:42:37 -0300

    feat: add dynamic data to DoughnutChart component

--- components/doughnut-chart.tsx
+++ components/doughnut-chart.tsx
@@ -4,16 +4,20 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
+
 export default function DoughnutChart({ accounts }: DoughnutChartProps) {
+  const accountNames = accounts.map((account) => account.name);
+  const balances = accounts.map((account) => account.currentBalance);
+
   const data = {
     datasets: [
       {
-        data: [1250, 2500, 3750],
+        data: balances,
         label: "Amount",
         backgroundColor: ["#00D4D5", "#009D9E", "#BFFCFB"],
       },
     ],
-    labels: ["Bank 1", "Bank 2", "Bank 3"],
+    labels: accountNames,
   };
 
   return (
