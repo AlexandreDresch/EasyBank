83144b993411a72c462b5a5ef01cd7ad12f6c527
AlexandreDresch <connectWithAlexandre@proton.me>
2024-06-12 23:48:30 -0300

    feat: get dynamic data about user & accounts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,24 @@
 import Header from "@/components/header";
+import RecentTransactions from "@/components/recent-transactions";
 import RightSidebar from "@/components/right-sidebar";
 import TotalBalance from "@/components/total-balance";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-export default async function Home() {
+export default async function Home({
+  searchParams: { id, page },
+}: SearchParamProps) {
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id });
+  const currentPage = Number(page as string) || 1;
+
+  if (!accounts) {
+    return;
+  }
+
+  const appwriteItemId = (id as string) || accounts?.data[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
 
   return (
     <section className="home">
@@ -13,22 +27,29 @@ export default async function Home() {
           <Header
             type="greeting"
             title="Welcome"
-            user={loggedIn?.name || "Guest"}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
 
           <TotalBalance
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={1000.5}
+            accounts={accounts?.data}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
+
+        <RecentTransactions
+          accounts={accounts?.data}
+          transactions={accounts?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
       </div>
 
       <RightSidebar
         user={loggedIn}
-        banks={[{ currentBalance: 578 }, { currentBalance: 1000 }]}
-        transactions={[]}
+        banks={accounts?.data.slice(0, 2)}
+        transactions={accounts?.transactions}
       />
     </section>
   );
