6b6c0d5d7814a68ce9b11814366c482ffe0a33da
AlexandreDresch <connectWithAlexandre@proton.me>
2024-06-12 23:42:15 -0300

    fix: correct user names usage

--- components/right-sidebar.tsx
+++ components/right-sidebar.tsx
@@ -15,12 +15,14 @@ export default function RightSidebar({
         <div className="profile">
           <div className="profile-img">
             <span className="text-4xl font-bold text-blue-400">
-              {user?.name[0]}
+              {user?.firstName[0]}
             </span>
           </div>
 
           <div className="profile-details">
-            <h1 className="profile-name">{user.name}</h1>
+            <h1 className="profile-name">
+              {user.firstName} {user.lastName}
+            </h1>
 
             <p className="profile-email">{user.email}</p>
           </div>
@@ -48,7 +50,7 @@ export default function RightSidebar({
               <BankCard
                 key={banks[0].$id}
                 account={banks[0]}
-                userName={user.name}
+                userName={`${user.firstName} ${user.lastName}`}
                 showBalance={false}
               />
             </div>
@@ -58,7 +60,7 @@ export default function RightSidebar({
                 <BankCard
                   key={banks[1].$id}
                   account={banks[1]}
-                  userName={user.name}
+                  userName={`${user.firstName} ${user.lastName}`}
                   showBalance={false}
                 />
               </div>
