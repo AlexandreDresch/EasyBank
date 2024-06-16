820bd08f5c9a696a058038ba8506ba8cc8bf8185
AlexandreDresch <connectWithAlexandre@proton.me>
2024-06-15 22:27:42 -0300

    feat: add MyBanks page

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,6 +1,36 @@
+import BankCard from "@/components/bank-card";
+import Header from "@/components/header";
+import { getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-export default function MyBanks() {
+export default async function MyBanks() {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id });
+
   return (
-    <div>page</div>
-  )
+    <section className="flex">
+      <div className="my-banks">
+        <Header
+          title="My Bank Accounts"
+          subtext="Effortlessly manage your banking activities."
+        />
+
+        <div className="space-y-4">
+          <h2 className="header-2">Your Cards</h2>
+
+          <div className="flex flex-wrap gap-6">
+            {accounts &&
+              accounts.data.map((account: Account) => (
+                <BankCard
+                  account={account}
+                  key={account.id}
+                  className="min-w-[325px]"
+                  userName={loggedIn?.firstName}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
