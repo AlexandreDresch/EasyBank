f1f13b08dae16b6a60081c44bbead5efafcd1975
AlexandreDresch <connectWithAlexandre@proton.me>
2024-06-17 23:58:35 -0300

    feat: update PaymentTransfer page

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,6 +1,21 @@
+import Header from "@/components/header";
+import PaymentTransferForm from "@/components/payment-transfer-form";
+import { getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-export default function PaymentTransfer() {
+export default async function PaymentTransfer() {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id });
   return (
-    <div>page</div>
-  )
+    <section className="payment-transfer">
+      <Header
+        title="Payment Transfer"
+        subtext="Please provide any specif details or notes related to the payment transfer."
+      />
+
+      <section className="size-full pt-5">
+        <PaymentTransferForm accounts={accounts?.data} />
+      </section>
+    </section>
+  );
 }
