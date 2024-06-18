a16ca94a471176d3e3a9d0a1a7475dc8270fbaca
AlexandreDresch <connectWithAlexandre@proton.me>
2024-06-17 21:57:10 -0300

    feat: add preview image to auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -7,13 +9,14 @@ export default function RootLayout({
     <main className="flex min-h-screen w-full justify-between font-inter">
       {children}
       <div className="auth-asset">
-        <div>
-          {/* <Image
-            src={"/preview.svg"}
-            width={500}
-            height={500}
+        <div className="pl-10 w-full flex items-center justify-end">
+          <Image
+            src="/preview.png"
+            width={700}
+            height={900}
+            className="rounded-l-xl object-contain"
             alt="EasyBank - Preview"
-          /> */}
+          />
         </div>
       </div>
     </main>
